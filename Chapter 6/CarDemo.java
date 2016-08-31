df8966c226d5b6670465e6c5affc23d944ebd7da
estherv93 <esthervelez93@gmail.com>
2016-08-31 11:48:55 -0400

    Answer Chapter 6 Questions

---
+++ Chapter 6/CarDemo.java
@@ -0,0 +1,39 @@
+// Question Summary: Page 338 - #2 (Car Class)
+
+package homework;
+
+public class CarDemo {
+
+	public static void main (String[] args)
+	{
+		// Create new car object
+		Car mercedes = new Car();
+		
+		// Set the Year Model
+		mercedes.setYearModel(1993);
+		
+		// Set the Make
+		mercedes.setMake("190E");
+		
+		// Get the Year Model, Make, and Speed of the Car object
+		System.out.println("Year Model: " + mercedes.getYearModel());
+		System.out.println("Make: " + mercedes.getMake());
+		System.out.println("Speed: " + mercedes.getSpeed());
+		
+		// Accelerate the speed 5 times
+		System.out.println("\nRunning Acceleration and Printing new speed 5x");
+		System.out.println("Current Speed is: " + mercedes.accelerate());
+		System.out.println("Current Speed is: " + mercedes.accelerate());
+		System.out.println("Current Speed is: " + mercedes.accelerate());
+		System.out.println("Current Speed is: " + mercedes.accelerate());
+		System.out.println("Current Speed is: " + mercedes.accelerate());
+		
+		// Brake the speed 5 times
+		System.out.println("\nBraking and Printing new speed 5x");
+		System.out.println("Current Speed is: " + mercedes.brake());
+		System.out.println("Current Speed is: " + mercedes.brake());
+		System.out.println("Current Speed is: " + mercedes.brake());
+		System.out.println("Current Speed is: " + mercedes.brake());
+		System.out.println("Current Speed is: " + mercedes.brake());
+	}
+}
---
+++ Chapter 6/EmployeeDemo.java
@@ -0,0 +1,41 @@
+// Question Summary: Page 338 - #1 (Employee Class)
+
+package homework;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args)
+	{
+		// Create Susan Employee object
+		Employee susan = new Employee();
+		susan.setName("Susan Meyers");
+		susan.setIdNumber(47899);
+		susan.setDepartment("Accounting");
+		susan.setPosition("Vice President");
+		
+		// Create Mark Employee object
+		Employee mark = new Employee();
+		mark.setName("Mark Jones");
+		mark.setIdNumber(39119);
+		mark.setDepartment("IT");
+		mark.setPosition("Programmer");
+		
+		// Create Joy Employee object
+		Employee joy = new Employee();
+		joy.setName("Joy Rogers");
+		joy.setIdNumber(81774);
+		joy.setDepartment("Manufacturing");
+		joy.setPosition("Engineer");
+		
+		// Create a chart to display the Employee objects
+		System.out.println("Name\t\t ID Number\t Department\t\t Position\t");
+		System.out.println("----------------------------------------------------------------");
+		System.out.println(susan.getName() + "\t " + susan.getIdNumber() 
+						+ "\t\t " + susan.getDepartment() + "\t\t " + susan.getPosition());
+		System.out.println(mark.getName() + "\t " + mark.getIdNumber() 
+						+ "\t\t " + mark.getDepartment() + "\t\t\t " + mark.getPosition());
+		System.out.println(joy.getName() + "\t " + joy.getIdNumber() 
+						+ "\t\t " + joy.getDepartment() + "\t\t " + joy.getPosition());
+		
+	}
+}
